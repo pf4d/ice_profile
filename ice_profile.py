3e1f13d2eba17cbc17ea4226a9ac5a58a2f472c2
Evan Cummings <evan.cummings@umontana.edu>
2013-05-31 09:59:52 -0600

    white space edit

--- ice_profile.py
+++ ice_profile.py
@@ -3,43 +3,43 @@ from dolfin import *
 import pylab 
 
 ### SIMULATION PARAMETERS ###
-dt = .01          # time step
-t = 0.            # begining time
-T = 50000.        # end time
-H_MIN = 1.        # Minimal ice thickness
+dt    = .01           # time step
+t     = 0.            # begining time
+T     = 50000.        # end time
+H_MIN = 1.            # Minimal ice thickness
 
 ### PHYSICAL CONSTANTS ###
-spy = 31556926.   # seconds per year
-rho = 911.        # density of ice (kg/m^3)
-rho_w = 1000.     # density of water (kg/m^3)
-g = 9.81 * spy**2 # gravitation acceleration (m/yr^2)
-n = 3             # flow law exponent
-B = 750.e3        # flow law temperature sensitivity factor (Pa*yr^.333)
-amax = .5         # max accumlation/ablation rate
-mu = 1.e16        # Basal traction constant
-p = 1.            # Basal sliding exponent
-q = 1.            # Basal sliding exponent 
+spy   = 31556926.     # seconds per year
+rho   = 911.          # density of ice (kg/m^3)
+rho_w = 1000.         # density of water (kg/m^3)
+g     = 9.81 * spy**2 # gravitation acceleration (m/yr^2)
+n     = 3             # flow law exponent
+B     = 750.e3        # flow law temperature sensitivity factor (Pa*yr^.333)
+amax  = .5            # max accumlation/ablation rate
+mu    = 1.e16         # Basal traction constant
+p     = 1.            # Basal sliding exponent
+q     = 1.            # Basal sliding exponent 
 
 ### DOMAIN DESCRIPTION ###
-xl = 0.           # left edge (divide)
-xr = 1000.e3      # right edge (margin/terminus)
-H0 = 20.          # thickness at divide
-L = xr - xl       # length of domain
+xl    = 0.            # left edge (divide)
+xr    = 1000.e3       # right edge (margin/terminus)
+H0    = 20.           # thickness at divide
+L     = xr - xl       # length of domain
 
 # Unit interval mesh
-mesh = IntervalMesh(50,xl,xr)
-h = CellSize(mesh)
-xcoord = mesh.coordinates()
+mesh  = IntervalMesh(50,xl,xr)
+h     = CellSize(mesh)
+coord = mesh.coordinates()
 
 # Create FunctionSpace
-Q = FunctionSpace(mesh, "CG", 1)
+Q     = FunctionSpace(mesh, "CG", 1)
 
 # Boundary conditions:
 def divide(x,on_boundary):
-    return on_boundary and x[0] < xl + 1.e-6
+  return on_boundary and x[0] < xl + 1.e-6
 
 def terminus(x,on_boundary):
-    return on_boundary and x[0] > xr - 1.e-6
+  return on_boundary and x[0] > xr - 1.e-6
 
 u_bc = DirichletBC(Q,0.,divide)
 H_bc = DirichletBC(Q,H_MIN,terminus)
@@ -47,27 +47,28 @@ H_bc = DirichletBC(Q,H_MIN,terminus)
 # INTIAL CONDITIONS:
 # Surface
 # This equilibrium profile comes from vanderVeen p. 126, eq 5.50
-p0 = 'H0 / pow(n-1,n/(2*n+2)) * pow(( (n+1) * x[0] / L'+\
-     '- 1 + n * pow(( 1 - x[0]  / L ),1+1/n) - n *  pow( x[0]/L,1+1/n)),n/(2*n+2))'
-zs= interpolate(Expression(p0,L=L,H0=H0,n=n),Q)
+p0   = 'H0 / pow(n-1,n/(2*n+2)) * pow(( (n+1) * x[0] / L'+\
+       '- 1 + n * pow(( 1 - x[0]  / L ),1+1/n) '+\
+       '- n *  pow( x[0]/L,1+1/n)),n/(2*n+2))'
+zs   = interpolate(Expression(p0,L=L,H0=H0,n=n),Q)
 # Bed
-zb = interpolate(Constant(0),Q)
+zb   = interpolate(Constant(0),Q)
 # Thickness
-H0 = project(zs-zb,Q)
+H0   = project(zs-zb,Q)
 
 # Half width
-W = interpolate(Constant(1000.),Q)
+W    = interpolate(Constant(1000.),Q)
 
 # Initial Velocity
-u0 = interpolate(Constant(0),Q) 
+u0   = interpolate(Constant(0),Q) 
 
 adot = Expression('amax * ( .75 - x[0] / L)',L=L,amax=amax)
 
 # Test and trial functions
-u, v = TrialFunction(Q), TestFunction(Q)      # for velocity
-H, phi =  TrialFunction(Q), TestFunction(Q)   # for thickness
-u_ = Function(Q)                              # most recent velocity solution
-H_ = Function(Q)
+u, v   = TrialFunction(Q), TestFunction(Q)   # for velocity
+H, phi = TrialFunction(Q), TestFunction(Q)   # for thickness
+u_     = Function(Q)                         # most recent velocity solution
+H_     = Function(Q)
 
 # Mid-point solution for stabalization
 H_mid = 0.5*(H0 + H_)
@@ -75,8 +76,7 @@ u_mid = 0.5*(u0 + u_)
 
 # Galerkin variational problem
 # Continuity equation
-FH = (H-H0) * phi * dx + ((2*W*u_mid*H).dx(0) - adot)\
-     * phi * dt * dx 
+FH = (H-H0) * phi * dx + ((2*W*u_mid*H).dx(0) - adot) * phi * dt * dx 
 
 # Momentum balance: weak form of equation xx of vanderveen
 #Fu = -rho * g * H0 * zs.dx(0) * v * dx \
@@ -86,20 +86,20 @@ FH = (H-H0) * phi * dx + ((2*W*u_mid*H).dx(0) - adot)\
 
 # Momentum balance with regularization on viscosity
 small = 1e-6
-Fu = -rho * g * H_mid * zs.dx(0) * v * dx \
-     -mu * (H_mid - rho_w / rho * zb)**q * u**p * v * dx\
-     +2. * B * H_mid * abs(u.dx(0)+small)**((1-n)/n)*u.dx(0) * v.dx(0) *dx\
-     -B * H_mid / W * (((n+2) * u)/(2*W))**(1/n) * v * dx
+Fu    = -rho * g * H_mid * zs.dx(0) * v * dx \
+        -mu * (H_mid - rho_w / rho * zb)**q * u**p * v * dx\
+        +2. * B * H_mid * abs(u.dx(0)+small)**((1-n)/n)*u.dx(0) * v.dx(0) *dx\
+        -B * H_mid / W * (((n+2) * u)/(2*W))**(1/n) * v * dx
 
-Fu = action(Fu,u_)   # Linearize about the most recent solution, u_
-J  = derivative(Fu, u_, u)  # Gateaux derivative of Fu at u_ in dir. of u
+Fu    = action(Fu,u_)          # Linearize about the most recent solution, u_
+J     = derivative(Fu, u_, u)  # Gateaux derivative of Fu at u_ in dir. of u
 
 # Residual for stabization of H only
-r = H-H0 + dt * (H * u_mid).dx(0) - adot * dt
+r     = H-H0 + dt * (H * u_mid).dx(0) - adot * dt
 
 # Add SUPG stabilisation terms
 unorm = sqrt(dot(u_mid, u_mid))
-FH += (h/(2.0*unorm))*dot(u_mid, u_mid.dx(0)) * r * dx
+FH   += (h/(2.0*unorm))*dot(u_mid, u_mid.dx(0)) * r * dx
 
 # Create bilinear and linear forms
 aH = lhs(FH)
@@ -121,9 +121,9 @@ u_problem = NonlinearVariationalProblem(Fu, u_, u_bc, J)
 u_solver  = NonlinearVariationalSolver(u_problem)
 
 prm = u_solver.parameters
-prm['newton_solver']['absolute_tolerance'] = 1E-8
-prm['newton_solver']['relative_tolerance'] = 1E-7
-prm['newton_solver']['maximum_iterations'] = 250
+prm['newton_solver']['absolute_tolerance']   = 1E-8
+prm['newton_solver']['relative_tolerance']   = 1E-7
+prm['newton_solver']['maximum_iterations']   = 250
 prm['newton_solver']['relaxation_parameter'] = 0.8
 
 u_solver.solve()
@@ -134,7 +134,7 @@ out_file = File("results/ice_profile.pvd")
 
 # Set intial condition
 #u0 = u_ 
-H = H0
+H  = H0
 # Plot solution
 plot(u_,title='Velocity')
 plot(H,title='Thickness')
@@ -143,33 +143,33 @@ raw_input("Press enter...")
 
 # Time-stepping
 while t < T:
-    # Assemble vector and apply boundary conditions
-    #b = assemble(LH)
-    #H_bc.apply(b)
+  # Assemble vector and apply boundary conditions
+  #b = assemble(LH)
+  #H_bc.apply(b)
 
-        # Solve the nonlinear system 
-    u_solver.solve()
+  # Solve the nonlinear system 
+  u_solver.solve()
 
-    # Solve the linear system 
-    #H_solver.solve(H.vector(),b)
+  # Solve the linear system 
+  #H_solver.solve(H.vector(),b)
 
-    H_solver.solve()
-    # Constrain to positive thickness
-    H_.vector()[H_.vector().array()<H_MIN] = H_MIN
+  H_solver.solve()
+  # Constrain to positive thickness
+  H_.vector()[H_.vector().array()<H_MIN] = H_MIN
 
 
-    # Copy solution from previous interval
-    H0 = H_ 
-    u0 = u_
+  # Copy solution from previous interval
+  H0 = H_ 
+  u0 = u_
 
-    # Plot solution
-    plot(u_,title='Velocity')
-    plot(H,title='Thickness')
+  # Plot solution
+  plot(u_,title='Velocity')
+  plot(H,title='Thickness')
 
-    # Save the solution to file
-    out_file << (H_, t)
-    out_file << (u_, t)
+  # Save the solution to file
+  out_file << (H_, t)
+  out_file << (u_, t)
 
-    # Move to next interval and adjust boundary condition
-    t += dt
-    #raw_input("Push enter to contiue")
+  # Move to next interval and adjust boundary condition
+  t += dt
+  #raw_input("Push enter to contiue")
