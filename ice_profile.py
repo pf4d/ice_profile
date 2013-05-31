c186eda52a5e13a1c1db2587b713535fd11c3b8f
Evan Cummings <evan.cummings@umontana.edu>
2013-05-31 17:58:17 -0600

    working in time

--- ice_profile.py
+++ ice_profile.py
@@ -24,9 +24,9 @@ mpl.rcParams['font.family']     = 'serif'
 mpl.rcParams['legend.fontsize'] = 'medium'
 
 ### SIMULATION PARAMETERS ###
-dt    = .01           # time step
+dt    = 5.0           # time step
 t     = 0.            # begining time
-T     = 50000.        # end time
+T     = 5000.         # end time
 H_MIN = 1.            # Minimal ice thickness
 
 ### PHYSICAL CONSTANTS ###
@@ -44,16 +44,17 @@ q     = 1.            # Basal sliding exponent
 ### DOMAIN DESCRIPTION ###
 xl    = 0.            # left edge (divide)
 xr    = 1000.e3       # right edge (margin/terminus)
-H0    = 20.           # thickness at divide
+H0    = 200.          # thickness at divide
 L     = xr - xl       # length of domain
 
 # Unit interval mesh
-mesh  = IntervalMesh(50,xl,xr)
-h     = CellSize(mesh)
+mesh  = IntervalMesh(500,xl,xr)
+cellh = CellSize(mesh)
 xcrd  = mesh.coordinates()
 
 # Create FunctionSpace
 Q     = FunctionSpace(mesh, "CG", 1)
+MQ    = Q * Q
 
 # Boundary conditions:
 def divide(x,on_boundary):
@@ -62,8 +63,8 @@ def divide(x,on_boundary):
 def terminus(x,on_boundary):
   return on_boundary and x[0] > xr - 1.e-6
 
-u_bc = DirichletBC(Q,0.,divide)
-H_bc = DirichletBC(Q,H_MIN,terminus)
+H_bc = DirichletBC(MQ.sub(0), H_MIN, terminus)  # height at terminus
+u_bc = DirichletBC(MQ.sub(1), 0.,    divide)    # velocity at divide
 
 # INTIAL CONDITIONS:
 # Surface
@@ -71,103 +72,96 @@ H_bc = DirichletBC(Q,H_MIN,terminus)
 p0   = 'H0 / pow(n-1,n/(2*n+2)) * pow(( (n+1) * x[0] / L'+\
        '- 1 + n * pow(( 1 - x[0]  / L ),1+1/n) '+\
        '- n *  pow( x[0]/L,1+1/n)),n/(2*n+2))'
+
+# Surface 
 zs   = interpolate(Expression(p0,L=L,H0=H0,n=n),Q)
+zt   = nan_to_num(zs.vector().array())
+zt[where(zt <= H_MIN)[0]] = H_MIN
+zs.vector().set_local(zt)
+
 # Bed
 zb   = interpolate(Constant(0),Q)
+
 # Thickness
-H0   = project(zs-zb,Q)
+H_i  = project(zs-zb,Q)
 
 # Half width
 W    = interpolate(Constant(1000.),Q)
 
 # Initial Velocity
-u0   = interpolate(Constant(0),Q) 
+u_i  = interpolate(Constant(0.0),Q) 
 
 adot = Expression('amax * ( .75 - x[0] / L)',L=L,amax=amax)
 
-# Test and trial functions
-u, v   = TrialFunction(Q), TestFunction(Q)   # for velocity
-H, phi = TrialFunction(Q), TestFunction(Q)   # for thickness
-u_     = Function(Q)                         # most recent velocity solution
-H_     = Function(Q)
+# variational problem :
+h        = Function(MQ)                      # solution
+H, u     = split(h)                          # solutions for H, u
+h0       = Function(MQ)                      # previous solution
+H0, u0   = split(h0)                         # initial value functions
+
+dh       = TrialFunction(MQ)                 # trial function for solution
+dH, du   = split(dh)                         # trial functions for H, u
+j        = TestFunction(MQ)                  # test function in mixed space
+phi, v   = split(j)                          # test functions for H, u
+
+h_i = project(as_vector([H_i,u_i]), MQ)      # project inital values on space
+h.vector().set_local(h_i.vector().array())   # initalize H, u in solution
+h0.vector().set_local(h_i.vector().array())  # initalize H, u in prev. sol
 
 # Mid-point solution for stabalization
-H_mid = 0.5*(H0 + H_)
-u_mid = 0.5*(u0 + u_)
+H_mid = 0.5*(H0 + H)
+theta = 1#0.878
+u_mid = theta*u + (1 - theta)*u0
 
-# Galerkin variational problem
+# SUPG method phihat :        
+unorm  = sqrt(dot(u, u) + 1e-10)
+phihat = phi + cellh/(2*unorm)*dot(u, phi.dx(0))
+
 # Continuity equation
-FH = (H-H0) * phi * dx + ((2*W*u_mid*H).dx(0) - adot) * phi * dt * dx 
+fH = (H-H0)/dt * phi * dx + ((2*W*u*H_mid).dx(0) - adot) * phihat * dx 
 
-# Momentum balance: weak form of equation xx of vanderveen
-#Fu = -rho * g * H0 * zs.dx(0) * v * dx \
-#     -mu * (H0 - rho_w / rho * zb)**q * u**p * v * dx\
-#     +2. * B * H0 * u.dx(0)**(1/n) * v.dx(0) *dx\
-#     -B * H0 / W * (((n+2) * u)/(2*W))**(1/n) * v * dx
+# Momentum balance: weak form of equation 9.65 of vanderveen
+fu = - rho * g * H0 * zs.dx(0) * v * dx - \
+       mu * (H0 - rho_w / rho * zb)**q * u_mid**p * v * dx + \
+       2. * B * H0 * u_mid.dx(0)**(1/n) * v.dx(0) * dx - \
+       B * H0 / W * (((n+2) * u_mid)/(2*W))**(1/n) * v * dx
 
 # Momentum balance with regularization on viscosity
-small = 1e-6
-Fu    = -rho * g * H_mid * zs.dx(0) * v * dx \
-        -mu * (H_mid - rho_w / rho * zb)**q * u**p * v * dx\
-        +2. * B * H_mid * abs(u.dx(0)+small)**((1-n)/n)*u.dx(0) * v.dx(0) *dx\
-        -B * H_mid / W * (((n+2) * u)/(2*W))**(1/n) * v * dx
+#small = 1e-6
+#fu    = -rho * g * H_mid * zs.dx(0) * v * dx \
+#        -mu * (H_mid - rho_w / rho * zb)**q * u**p * v * dx\
+#        +2. * B * H_mid * abs(u.dx(0)+small)**((1-n)/n)*u.dx(0) * v.dx(0) *dx\
+#        -B * H_mid / W * (((n+2) * u)/(2*W))**(1/n) * v * dx
 
-Fu    = action(Fu,u_)          # Linearize about the most recent solution, u_
-J     = derivative(Fu, u_, u)  # Gateaux derivative of Fu at u_ in dir. of u
-
-# Residual for stabization of H only
-r     = H-H0 + dt * (H * u_mid).dx(0) - adot * dt
-
-# Add SUPG stabilisation terms
-unorm = sqrt(dot(u_mid, u_mid))
-FH   += (h/(2.0*unorm))*dot(u_mid, u_mid.dx(0)) * r * dx
-
-# Create bilinear and linear forms
-aH = lhs(FH)
-LH = rhs(FH)
-
-# Assemble matrix
-#AH = assemble(aH)
-#H_bc.apply(AH)
-
-# Create linear solver and factorize matrix
-#H_solver = LUSolver(AH)
-#H_solver.parameters["reuse_factorization"] = False
-
-H_problem = LinearVariationalProblem(aH,LH,H_,H_bc)
-H_solver  = LinearVariationalSolver(H_problem)
+f     = fH + fu
+df    = derivative(f, h, dh)
 
 # Create non-linear solver instance
-u_problem = NonlinearVariationalProblem(Fu, u_, u_bc, J)
-u_solver  = NonlinearVariationalSolver(u_problem)
+problem = NonlinearVariationalProblem(f, h, [H_bc, u_bc], J=df)
+solver  = NonlinearVariationalSolver(problem)
 
-prm = u_solver.parameters
+prm = solver.parameters
 prm['newton_solver']['absolute_tolerance']   = 1E-8
 prm['newton_solver']['relative_tolerance']   = 1E-7
 prm['newton_solver']['maximum_iterations']   = 250
 prm['newton_solver']['relaxation_parameter'] = 0.8
 
-u_solver.solve()
-H_solver.solve()
+solver.solve()
 
 # Output file
 out_file = File("results/ice_profile.pvd")
 
-# Set intial condition
-#u0 = u_ 
-H  = H0
-
 # Plot solution
 plt.ion()
-Hplot = project(H,  Q).vector().array()
-uplot = project(u_, Q).vector().array()
+Hplot = project(H, Q).vector().array()
+uplot = project(u, Q).vector().array()
 
-fig = plt.figure()
+fig = plt.figure(figsize=(10,6))
 ax1 = fig.add_subplot(111)
 hp, = ax1.plot(xcrd, Hplot, 'k', lw=2)
 ax1.set_xlabel('$x$ [m]')
 ax1.set_ylabel('$H$ [m]')
-ax1.set_ylim([0,20])
+ax1.set_ylim([0,2000])
 
 ax2 = ax1.twinx()
 gry = '0.4'
@@ -190,26 +184,28 @@ while t < T:
   #H_bc.apply(b)
 
   # Solve the nonlinear system 
-  u_solver.solve()
-
-  # Solve the linear system 
-  #H_solver.solve(H.vector(),b)
-  H_solver.solve()
+  solver.solve()
 
   # Copy solution from previous interval
-  H0 = H_ 
-  u0 = u_
+  h0.assign(h)
 
   # Plot solution
-  Hplot = project(H,  Q).vector().array()
-  uplot = project(u_, Q).vector().array()
+  Hplot = project(H, Q).vector().array()
+  uplot = project(u, Q).vector().array()
+  Hplot[where(Hplot < H_MIN)[0]] = H_MIN
+  
+  # update the dolfin vectors :
+  H_i.vector().set_local(Hplot)
+  h_new = project(as_vector([H_i, u]), MQ)
+  h.vector().set_local(h_new.vector().array())
+
   hp.set_ydata(Hplot)
   up.set_ydata(uplot)
   plt.draw() 
 
   # Save the solution to file
-  out_file << (H_, t)
-  out_file << (u_, t)
+  #out_file << (H, t)
+  #out_file << (u, t)
 
   # Move to next interval and adjust boundary condition
   t += dt
