f7e8252237b08b36ecb2a1af4eedb7a713d2d145
Evan Cummings <evan.cummings@umontana.edu>
2013-07-23 12:40:33 -0600

    enforced non-negativity

--- manual_newton.py
+++ manual_newton.py
@@ -24,14 +24,14 @@ bcs = [DirichletBC(MQ.sub(0), 1,   left),
 
 k   = 1.0*u1 + 1.0
 
-F1  = sqrt(k) * u1.dx(0) * t1.dx(0) * dx
+F1  = k * sqrt(u1.dx(0)) * t1.dx(0) * dx
 F2  = u2 * t2 * dx
 F   = F1 + F2
 
 Jac = derivative(F, U, dU)
 
 u1_init = Expression("1-x[0]")
-u2_init = Constant(0.0)
+u2_init = Constant(1E-18)
 
 U_k = project(as_vector([u1_init, u2_init]), MQ) # project inital values
 U.vector().set_local(U_k.vector().array())       # initalize u1, u2 in solution
@@ -42,26 +42,40 @@ if False:
   solver = NonlinearVariationalSolver(problem)
   solver.solve()
 else:
-  a_tol, r_tol = 1e-7, 1e-10
-  U_inc   = Function(MQ)
-  bcs     = homogenize(bcs)
-  nIter   = 0
-  eps     = 1
-  maxIter = 200
+  a_tol, r_tol = 1e-7, 1e-10                     # abs/rel tolerances
+  U_inc   = Function(MQ)                         # residual
+  bcs     = homogenize(bcs)                      # residual is zero on boundary
+  nIter   = 0                                    # number of iterations
+  eps     = 1                                    # initial epsilon
+  maxIter = 200                                  # max iterations
 
   while eps > r_tol and nIter < maxIter:         # Newton iterations
-    nIter += 1
-    A, b   = assemble_system(Jac, -F, bcs)
-    solve(A, U_inc.vector(), b)                  # Determine step direction
-    eps    = U_inc.vector().norm('l2')
+    nIter += 1                                   # increment interation
+    A, b   = assemble_system(Jac, -F, bcs)       # assemble system
+    solve(A, U_inc.vector(), b)                  # determine step direction
+    eps    = U_inc.vector().norm('l2')           # calculate norm
 
     a     = assemble(F)
     for bc in bcs:
       bc.apply(a)
     fnorm = b.norm('l2')
     lmbda = 1.0                                  # step size, initially 1
-
+    
     U.vector()[:] += lmbda*U_inc.vector()        # New u vector
+
+    # ensure non-negativity :
+    u1_t = project(u1, Q).vector().array()
+    u2_t = project(u2, Q).vector().array()
+
+    u1_t[u1_t <= 0] = 1E-18
+    u2_t[u2_t <= 0] = 1E-18
+
+    u1_ = Function(Q)
+    u1_.vector().set_local(u1_t)
+    u2_ = Function(Q)
+    u2_.vector().set_local(u2_t)
+    U_new = project(as_vector([u1_, u2_]), MQ)
+    U.vector().set_local(U_new.vector().array())
 
     print '      {0:2d}  {1:3.2E}  {2:5e}'.format(nIter, eps, fnorm)
 
